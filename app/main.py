b989ea9e3978a691f66d57285fc3d55ee19494b8
romagudavadze <roma.gudavadze@gmail.com>
2025-04-16 20:40:55 +0400

    feat: modify auth and account routers

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import Base, engine
+from api.auth import router as auth_router
+from api.account import router as account_router
+from database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="QuickBooks Integration API")
+
+app.include_router(account_router)
+app.include_router(auth_router)
+
 
 app.add_middleware(
     CORSMiddleware,
